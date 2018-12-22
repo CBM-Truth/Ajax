4bd0fc0abef253f21eba1d36511a50898d1bdef8
CBM-Truth <cameronmeissner@gmail.com>
2018-12-22 02:16:40 -0800

    Updated file spacing.

--- Ajax/src/primary/AjaxDriver.java
+++ Ajax/src/primary/AjaxDriver.java
@@ -6,13 +6,13 @@ public class AjaxDriver {
 	
 	public static void main(String[] args) { 
 		Ajax graph = new Ajax();
-		graph.addEdge("5", "2");
-		graph.addEdge("5", "0");
-		graph.addEdge("4", "0");
-		graph.addEdge("4", "1");
+		graph.addEdge("1", "2");
 		graph.addEdge("2", "3");
-		graph.addEdge("3", "1");
-		graph.topoSortPrint("5");
+		graph.addEdge("1", "3");
+		graph.addEdge("2", "4");
+		graph.addEdge("3", "4");
+		graph.addEdge("3", "5");
+		graph.topoSortPrint("1");
 	}
 
 }
