07808ff7a5b28ce57c95716179bd8a43400ad2f5
CBM-Truth <cameronmeissner@gmail.com>
2018-12-27 13:33:36 -0800

    Optimized imports.

--- Ajax/src/primary/Ajax.java
+++ Ajax/src/primary/Ajax.java
@@ -1,7 +1,12 @@
 package primary;
 
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Ajax<V> {
 
